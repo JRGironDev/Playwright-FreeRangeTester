4b79d41f345454a208af1eb2c92a73fb0e531dd1
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-02-16 17:21:01 -0600

    Agregar pruebas iniciales y variables de ejemplo en test-1.spec.ts

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,9 @@
+let edad : number = 20;
+let altura: number = 1.80;
+
+let nombre: string = "Juan";
+let apellido: string = "Quinonez";
+
+let estaActivo: boolean = true;
+
+let nombres: string[] = ["Juan", "Pedro", "Daniela"];
