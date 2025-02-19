6c24f6ccc3685bcf19de949d66fff307f31b4ca0
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-02-19 03:12:23 -0600

    Agregar pruebas de navegación para la sección de cursos en navigation.spec.ts

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -4,9 +4,16 @@ test.describe('Navigation', () => {
     test('navigation to https://www.freerangetesters.com', async ({ page }) => {
         await test.step('navigation to https://www.freerangetesters.com', async () => {
             await page.goto('https://www.freerangetesters.com/');
+            await expect(page).toHaveTitle(/Free Range Testers/);
         });
 
-        // Expect a title "to contain" a substring.
-        //await expect(page).toHaveTitle(/Free Range Testers/);
+        await test.step('Cuando haga click en "Cursos"', async () => {
+            page.locator('#page_header').getByRole('link', { name: 'Cursos', exact: true }).click();
+            await page.waitForURL('**/cursos');
+        });
+
+        await test.step('Soy dirigido a la sección de cursos', async () => {
+            await expect(page).toHaveTitle('Cursos');
+        });
     });
 });
