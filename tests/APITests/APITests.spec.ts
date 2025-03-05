f25a5d7e32316c1c6a2c0875688ddad4d078fc5f
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-03-05 16:23:06 -0600

    Actualizar pruebas de API para usar URL completa en las solicitudes y modificar los títulos de los issues creados

--- tests/APITests/APITests.spec.ts
+++ tests/APITests/APITests.spec.ts
@@ -19,27 +19,10 @@ test('Se puede crear un Issue en el repositrio de GitHub ', async ({ request }) 
 
     expect(newIssue.status()).toBe(201);
 
-    const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+    const issues = await request.get(`https://api.github.com/repos/${USER}/${REPO}/issues`);
     expect(issues.status()).toBe(200);
     expect(await issues.json()).toContainEqual(expect.objectContaining({
-        title: '[BUG] reporte 1',
-        body: 'Este es un reporte de un bug',
+        title: 'Issue creado desde Playwright',
+        body: 'Este issue fue creado desde Playwright',
     }));
 }); 
-
-test('Puedo crear un bug en el repo', async ({ request }) => {
-    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-        data: {
-            title: '[Bug] Explotó todo',
-            body: 'Estamos perdidirijillos!',
-        }
-    });
-    expect(newIssue.ok()).toBeTruthy();
- 
-    const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-    expect(issues.ok()).toBeTruthy();
-    expect(await issues.json()).toContainEqual(expect.objectContaining({
-        title: '[Bug] Explotó todo',
-        body: 'Estamos perdidirijillos!'
-    }));
-});
